3bc59834ed1e75386075c28d03d9321930234e1b
Rita Abou Fares <rja62@mail.aub.edu>
2026-06-22 12:22:31 +0300

    main commit

---
+++ backend/ECommerce.Application/DTOs/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ECommerce.Application.Dtos;
+
+public class ProductDto()
+{public int Id { get; set; }
+    public string Sku { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public bool IsInStock { get; set; }
+
+
+}
---
+++ backend/ECommerce.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ECommerce.Domain.Entities;
+{
+	public class Product()
+	{
+	public int Id { get; set; }
+	public string Sku { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
+	public string? ImageUrl { get; set; }
+	public decimal price { get; set; }
+	public int StockQuantity { get; set; }
+
+	public bool isActive { get; set; } = true;
+	}
+}
