b7723551bfc9d10a1cce1045f8f8f44223306802
Rawad Naameh <rawad.naameh@mobileartsme.com>
2026-06-23 15:23:49 +0300

    commit changes

--- backend/ECommerce.API/Program.cs
+++ backend/ECommerce.API/Program.cs
@@ -1,7 +1,13 @@
+using ECommerce.Application.Interfaces;
 using ECommerce.Infrastructure;
+using ECommerce.Infrastructure.Repositories;
 using ECommerce.Service;
 var builder = WebApplication.CreateBuilder(args);
 const string FrontendCorsPolicy = "FrontendCors";
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
