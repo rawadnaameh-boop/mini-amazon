d90454001d553a8eed39395aa4248d1d37881657
Rita Abou Fares <rja62@mail.aub.edu>
2026-06-24 14:55:17 +0300

    saving

---
+++ backend/ECommerce.Infrastructure/Migrations/20260624104620_AddCartOrders.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCartOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
