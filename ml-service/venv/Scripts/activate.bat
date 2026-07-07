aad72f13c7a988f7fe0881d3765289a8607d0547
Rita Abou Fares <rja62@mail.aub.edu>
2026-07-07 14:00:47 +0300

    commit

--- ml-service/venv/Scripts/activate.bat
+++ ml-service/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Rawad Naameh\Desktop\mini-amazon\ml-service\venv"
+set "VIRTUAL_ENV=C:\Users\Rita Abou Fares\source\repos\mini-amazon\ml-service\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
